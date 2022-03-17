4880cafe667d994540fa4644a4a21e4efe79684d
Andrei Brandes <andrei@lusha.com>
2022-03-17 15:39:48 +0200

    pr

---
+++ terraform/prod/main.tf
@@ -0,0 +1,10 @@
+variable "allowed_account_id" {
+  description = "The id  of the one AWS account this code is permitted to run against"
+  type        = string
+}
+
+provider "aws" {
+  region              = "eu-north-1"
+  allowed_account_ids = [var.allowed_account_id]
+}
+
