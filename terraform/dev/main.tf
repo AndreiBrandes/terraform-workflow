00664be7b2754230a1987a9da553b1de9ebae3cd
AndreiBrandes <66786858+AndreiBrandes@users.noreply.github.com>
2022-03-18 14:50:42 +0200

    Update main.tf

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -15,9 +15,4 @@ terraform {
     key    = "default-infrastructure"
     region = "eu-north-1"
   }
-# }
-
-# resource "aws_s3_bucket" "terraform_state" {
-#   bucket = "andrei-terraform-state"
-  
-# }
+}
