b840cc97131b4c46c62150543504cc761e7f9029
Andrei Brandes <andrei@lusha.com>
2022-03-18 15:55:41 +0200

    eks

--- terraform/dev/eks.tf
+++ terraform/dev/eks.tf
@@ -14,18 +14,11 @@ module "eks" {
   worker_groups = [
     {
       name                          = "worker-group-1"
-      instance_type                 = "t2.small"
+      instance_type                 = "t2.large"
       additional_userdata           = "echo foo bar"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
       asg_desired_capacity          = 2
-    },
-    {
-      name                          = "worker-group-2"
-      instance_type                 = "t2.small"
-      additional_userdata           = "echo foo bar"
-      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
-      asg_desired_capacity          = 1
-    },
+    }
   ]
 }
 
